ba5758268bb5213c0cc7d6e67470083b8966433c
Ravinder94 <karnatiravinder1994@gmail.com>
2021-03-22 00:45:15 +0530

    Web module for internationalization exampled, added hi_IN properties file

---
+++ Internationalization-web/src/main/java/Internationalization/web/constants/InternationalizationWebPortletKeys.java
@@ -0,0 +1,11 @@
+package Internationalization.web.constants;
+
+/**
+ * @author vidyayug-d18
+ */
+public class InternationalizationWebPortletKeys {
+
+	public static final String INTERNATIONALIZATIONWEB =
+		"Internationalization_web_InternationalizationWebPortlet";
+
+}
